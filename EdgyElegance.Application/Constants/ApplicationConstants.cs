c007084ae6c69c45462c0bf1f3b2f1e5d9fcdb79
Criss Guinther <crissguinther@proton.me>
2023-12-03 12:56:39 -0300

    feat: Get connection string for environment variable
    Gets the connection string from the environment variable, so the code gets easier to deploy with it

---
+++ EdgyElegance.Application/Constants/ApplicationConstants.cs
@@ -0,0 +1,5 @@
+﻿namespace EdgyElegance.Application.Constants;
+
+public class ApplicationConstants {
+    public static string CONNECTION_STRING { get => Environment.GetEnvironmentVariable("EdgyEleganceConnectionString") ?? string.Empty; } 
+}
